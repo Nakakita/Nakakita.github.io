65de65f19745888465f963de389ba96be48ef90e
limithand <limithand@gmail.com>
2016-09-18 08:42:53 +0900

    お題

--- api/word.php
+++ api/word.php
@@ -1,11 +1,36 @@
 <?php
 
 $words = [
-	'サランラップ',
-	'コンドーム',
-	mt_rand(1, 4),
+	[
+		'ポテチ',
+		'とんがりコーン',
+	],
+	[
+		'紅茶',
+		'コーヒー',
+	],
+	[
+		'夏祭り',
+		'花火',
+	],
+	[
+		'野球',
+		'サッカー',
+	],
+	[
+		'猫',
+		'犬',
+	],
 ];
 
+$word = $words[mt_rand(0, count($words) - 1)];
+$word[] = mt_rand(0, 3);
+
+$word = [
+	'野球',
+	'サッカー',
+	'2'
+];
 header("Access-Control-Allow-Origin: *");
 header('content-type: application/json; charset=utf-8');
-echo json_encode($words);
+echo json_encode($word);
