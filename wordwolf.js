369d5a62f2b8cb19c47c0564529bcefbd906d7da
nakakita tomoya <nakaki0217@gmail.com>
2016-09-17 16:58:19 +0900

    multiparty add

---
+++ wordwolf.js
@@ -0,0 +1,40 @@
+// SkyWayのシグナリングサーバーへ接続する (APIキーを置き換える必要あり）
+//var peer = new Peer({ key: '3fe45e9c-4406-41e7-a3db-0b45be57420c', debug: 3});
+
+var multiparty;
+
+function start(){
+    //Multiparty インスタンス
+    multiparty = new MultiParty({
+        "key":"3fe45e9c-4406-41e7-a3db-0b45be57420c",
+        "reliable":true,
+        "debug":3
+    });
+
+    //for MediaStream
+    multiparty.on('my_ms',function(video){
+        //自分のvideoを表示
+        var vNode = MultiParty.util.createVideoNode(video);
+        vNode.volume = 0;
+        $(vNode).appendTo('#streams');
+    }).on('peer_ms', function(video){
+        //pperのvideoを表示
+        var vNode = MultiParty.util.createVideoNode(video);
+        $(vNode).appendTo('#streams');
+    }).on('ms_close', function(peer_id){
+        //peerが切れたら、対象のvideoノードを削除する
+        $('#'+peer_id).remove();
+    })
+
+    $('button').on('click', function(ev){
+        multiparty.send('hello');/* 接続中のピアにメッセージを送信 */
+    });
+
+    multiparty.on('message', function(mesg){
+        $('p.receive').append(mesg.data + "<br>");/* 相手から受信したメッセージを表示 */
+    });
+
+    multiparty.start();
+}
+
+start();
