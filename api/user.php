f56d95d1d65f82fd9ac531b0a7e3dfbea5a9d6ba
limithand <limithand@gmail.com>
2016-09-17 23:24:53 +0900

    bugfix

--- api/user.php
+++ api/user.php
@@ -15,10 +15,7 @@ try{
 $sky_id = filter_input(INPUT_POST, 'sky_id');
 
 // INSERT
-stmt = $pdo -> prepare("INSERT INTO users (sky_id, created_at) VALUES (:sky_id, :now)");
+$stmt = $pdo->prepare("INSERT INTO users (sky_id, created_at) VALUES (:sky_id, :now)");
 $stmt->bindParam(':sky_id', $sky_id, PDO::PARAM_STR);
 $stmt->bindValue(':now', date('yyyy-mm-dd HH:ii:ss'), PDO::PARAM_STR);
 $stmt->execute();
-
-
-
